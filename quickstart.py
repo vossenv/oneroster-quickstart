4fbaf920b15852fdc1335865b90de2141d8b183c
jansseneda-adobe <danimaejanssen@gmail.com>
2019-08-05 23:55:06 -0500

    base version

---
+++ quickstart.py
@@ -0,0 +1,46 @@
+from pprint import pprint
+import oneroster
+
+connector = oneroster.ClasslinkConnector(
+    host_name="https://example.oneroster.com/ims/oneroster/v1p1/",
+    client_id='your_client_id',
+    client_secret='your_client_secret',
+)
+
+user_list = connector.get_users(user_filter='students')
+
+print(str(len(user_list)) + " students in total.  First result:\n")
+pprint(user_list[0])
+
+class_name = "ELA 6 (6A ELA)"
+ela_users = connector.get_users(user_filter='students',
+                                group_filter='classes',
+                                group_name=class_name)
+
+print("")
+print(str(len(ela_users)) + " users found for ELA 6")
+
+class_id = "31763"
+ela_users = connector.get_users(user_filter='students',
+                                group_filter='classes',
+                                group_name=class_id)
+
+print(str(len(ela_users)) + " users found for ELA 6 (1 section)")
+
+hschool_users = connector.get_users(user_filter='students',
+                                    group_filter='schools',
+                                    group_name='Classlink HS')
+
+mschool_users = connector.get_users(user_filter='students',
+                                    group_filter='schools',
+                                    group_name='7')
+
+print(str(len(hschool_users)) + " high school students returned")
+print(str(len(mschool_users)) + " middle school students returned")
+
+r302_users = connector.get_users(user_filter='students',
+                                 group_filter='classes',
+                                 group_name='302',
+                                 match_on='location')
+
+print(str(len(r302_users)) + " students with classes in room 302")
---
+++ quickstart_clever.py
@@ -0,0 +1,45 @@
+from pprint import pprint
+import oneroster
+
+connector = oneroster.CleverConnector(
+    host_name="https://api.clever.com/v2.1/",
+    access_token="TEST_TOKEN"
+)
+
+user_list = connector.get_users(user_filter='students')
+
+print(str(len(user_list)) + " students in total.  First result:\n")
+pprint(user_list[0])
+
+class_name = "Fine Arts, Class 703 - Ortiz - 7"
+ela_users = connector.get_users(user_filter='students',
+                                group_filter='sections',
+                                group_name=class_name)
+
+print("")
+print(str(len(ela_users)) + " users found for Fine Arts, Class 703 - Ortiz - 7")
+
+class_id = "58da8c6a894273be680001d8"
+ela_users = connector.get_users(user_filter='students',
+                                group_filter='sections',
+                                group_name=class_id)
+
+print(str(len(ela_users)) + " users found for Fine Arts, Class 703 - Ortiz - 7 by Id")
+
+hschool_users = connector.get_users(user_filter='students',
+                                    group_filter='schools',
+                                    group_name='Pineapple Elementary School')
+
+mschool_users = connector.get_users(user_filter='students',
+                                    group_filter='schools',
+                                    group_name='58da8c58155b940248000008')
+
+print(str(len(hschool_users)) + " elementary school students returned")
+print(str(len(mschool_users)) + " middle school students returned")
+
+r302_users = connector.get_users(user_filter='students',
+                                 group_filter='sections',
+                                 group_name='6',
+                                 match_on='grade')
+
+print(str(len(r302_users)) + " students in grade 6")
